56f75ba8dc84a296baf12fa4ff562db79fc0e3d3
Namaste Hacker <95855028+NamasteAbhi@users.noreply.github.com>
2024-02-04 05:30:07 +0530

    Update Device_Gen_Example.py

--- Device_Gen_Example.py
+++ Device_Gen_Example.py
@@ -1,12 +1,19 @@
 import requests
+import os
+import uuid
 
 url = "https://tiktok-device-registeration.p.rapidapi.com/Tiktok_Device_Gen/"
 
-rapid_key='' 
+rapid_key='' #Enter Your RapidApi_Key
 
 querystring = {
   "Proxy":"username:password@host:port",
-  "Country":"us"
+  "Country":"us",
+ 'Openudid': os.urandom(8).hex(), #Use Your Same Old Device Data For Reactivate Your Old Devices
+ 'req_id': str(uuid.uuid4()),
+ 'cdid':  str(uuid.uuid4()),
+ 'clientuuid': str(uuid.uuid4()),
+ 'google_aid':  str(uuid.uuid4()),
 }
 
 headers = {
@@ -20,5 +27,4 @@ response = requests.get(url, headers=headers, params=querystring)
 print(response.json())
 
 
-#{"is_activated": "success", "Device_Info": {"iid": "7280522761154561834", "device_id": "7280522557017884206", "passport-sdk-version": "19", "ac": "wifi", "channel": "googleplay", "aid": "1233", "app_name": "musical_ly", "version_code": "2023103030", "version_name": "31.3.3", "device_platform": "android", "os": "android", "ab_version": "31.3.3", "ssmix": "a", "device_type": "wiko", "device_brand": "WIKO_QYGPU", "language": "en", "os_api": "31", "os_version": "12", "openudid": "a0f94634068ad15b", "manifest_version_code": "2023103030", "resolution": "720*1280", "dpi": "240", "update_version_code": "2023103030", "app_type": "normal", "sys_region": "US", "mcc_mnc": "310240", "timezone_name": "America/New_York", "timezone_offset": -14400, "build_number": "31.3.3", "region": "US", "carrier_region": "US", "uoo": "0", "app_language": "en", "locale": "en", "op_region": "US", "ac2": "wifi", "host_abi": "armeabi-v7a", "cdid": "d0476399-dcfc-448c-b3d4-76267e8434e0", "support_webview": "1", "okhttp_version": "4.2.152.10-tiktok", "use_store_region_cookie": "1"}, "Cookies": {"store-idc": "useast5", "store-country-code": "us", "store-country-code-src": "did", "install_id": "7280522761154561834", "ttreq": "1$0b0a98bbd96a14f30dde1ccc8d1b0ba6da974ade"}, "secDeviceIdToken": "AqTsJsVFEWZ6_daXzvUni_KDd", "Seed_Algorithm": 10, "Seed_Token": "MDGnGJraqnIBIDd+yTwxwdWwx+B7Tn5yATB8pKsHSUmDIRbTETVtFlcqEBy9NQSm2egOIwHEyhqZuQ7OUKWmUQHGxzCKVrQFLdtnmhNLujRq/McdFrrprn0TNCuIu4SaAvE=", "Ri_Report": true}
-
+#{'Cookies': {'install_id': '7331528878471153454', 'store-country-code': 'us', 'store-country-code-src': 'did', 'store-idc': 'useast5', 'ttreq': '1$0f19d7c50fc0eb3093a52a300802452de901de35'}, 'Device_Info': {'Mssdk_Endpoint': 'mssdk16-normal-useast8.us.tiktokv.com', 'ab_version': '33.3.2', 'ac': 'wifi', 'ac2': 'wifi', 'aid': '1233', 'app_language': 'en', 'app_name': 'musical_ly', 'app_type': 'normal', 'build_number': '33.3.2', 'carrier_region': 'US', 'cdid': 'ff806113-c87f-4540-a330-a226d44fb098', 'channel': 'googleplay', 'clientuuid': '15775b02-475e-4074-9c55-c3b6001fb6ad', 'device_brand': 'realme', 'device_id': '7331528586270688810', 'device_platform': 'android', 'device_type': 'REALME_BRWST', 'dpi': '320', 'google_aid': '1c523255-5ca8-4bc8-a49e-30a56d1bd358', 'host_abi': 'armeabi-v7a', 'iid': '7331528878471153454', 'language': 'en', 'locale': 'en', 'manifest_version_code': '2023303020', 'mcc_mnc': '310650', 'okhttp_version': '4.2.137.48-tiktok', 'op_region': 'US', 'openudid': '53e32ad2028c24b9', 'os': 'android', 'os_api': '32', 'os_version': '12', 'passport-sdk-version': '19', 'region': 'US', 'req_id': '5474deec-ee9c-4a7f-8575-02454134ff1d', 'resolution': '1080*1920', 'ssmix': 'a', 'support_webview': '1', 'sys_region': 'US', 'timezone_name': 'America/New_York', 'timezone_offset': '-18000', 'uoo': '0', 'update_version_code': '2023303020', 'use_store_region_cookie': '1', 'user-agent': 'com.zhiliaoapp.musically/2023303020 (Linux; U; Android 12; en_US; REALME_BRWST; Build/N2G48H;tt-ok/3.12.13.4-tiktok)', 'version_code': '330302', 'version_name': '33.3.2'}, 'Ri_Report': True, 'Seed_Algorithm': 10, 'Seed_Token': 'MDGnEpnXqHIHLHMuhTpow4SxnLB4GyRwATUrq/taHhrUfRyBEDc/Qgd+QQOxNQDt2OkHJgTAxBLWvw7MHqf8UAWXwS6PSOYCYt0plxVeviA2rdwWWrnhpXoSYnmN7ICsZPM=', 'is_activated': 'success', 'secDeviceIdToken': 'AZoWhheN-3RAfIQzmqB4_icaj'}
